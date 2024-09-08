c5a0bdf644d990409bb221e87ac1c7fb7bf50831
Frederick Gnodtke <frederick@gnodtke.net>
2024-09-08 20:28:30 +0200

    Add sensors: Locked, Doors Locked, Doors, Windows, Trunk, Bonnet, Lights

--- binary_sensor.py
+++ binary_sensor.py
@@ -9,7 +9,7 @@ from .const import DATA_COODINATOR, DOMAIN
 from homeassistant.components.binary_sensor import (
     BinarySensorEntityDescription,
     BinarySensorEntity,
-    BinarySensorDeviceClass
+    BinarySensorDeviceClass,
 )
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import PERCENTAGE, UnitOfPower, UnitOfLength
@@ -39,6 +39,13 @@ async def async_setup_entry(
     for vehicle in vehicles:
         entities.append(EnyaqBinarySensorChargerConnected(coordinator, vehicle))
         entities.append(EnyaqBinarySensorChargerLocked(coordinator, vehicle))
+        entities.append(EnyaqBinarySensorLocked(coordinator, vehicle))
+        entities.append(EnyaqBinarySensorDoorsLocked(coordinator, vehicle))
+        entities.append(EnyaqBinarySensorDoorsOpen(coordinator, vehicle))
+        entities.append(EnyaqBinarySensorWindowsOpen(coordinator, vehicle))
+        entities.append(EnyaqBinarySensorTrunkOpen(coordinator, vehicle))
+        entities.append(EnyaqBinarySensorBonnetOpen(coordinator, vehicle))
+        entities.append(EnyaqBinarySensorLightsOn(coordinator, vehicle))
 
     async_add_entities(entities, update_before_add=True)
 
@@ -55,6 +62,7 @@ class EnyaqBinarySensor(EnyaqDataEntity, BinarySensorEntity):
         super().__init__(coordinator, vehicle, entity_description)
         BinarySensorEntity.__init__(self)
 
+
 class EnyaqBinarySensorChargerConnected(EnyaqBinarySensor):
     def __init__(self, coordinator: DataUpdateCoordinator, vehicle: Vehicle) -> None:
         super().__init__(
@@ -63,7 +71,7 @@ class EnyaqBinarySensorChargerConnected(EnyaqBinarySensor):
             BinarySensorEntityDescription(
                 key="charger_connected",
                 name=f"{vehicle.info.title} Charger Connected",
-                device_class= BinarySensorDeviceClass.PLUG,
+                device_class=BinarySensorDeviceClass.PLUG,
             ),
         )
         self._attr_unique_id = f"{vehicle.info.vin}_charger_connected"
@@ -89,6 +97,7 @@ class EnyaqBinarySensorChargerConnected(EnyaqBinarySensor):
         else:
             return "mdi:power-plug"
 
+
 class EnyaqBinarySensorChargerLocked(EnyaqBinarySensor):
     def __init__(self, coordinator: DataUpdateCoordinator, vehicle: Vehicle) -> None:
         super().__init__(
@@ -96,8 +105,8 @@ class EnyaqBinarySensorChargerLocked(EnyaqBinarySensor):
             vehicle,
             BinarySensorEntityDescription(
                 key="charger_locked",
-                name=f"{vehicle.info.title} Charger Locked",
-                device_class= BinarySensorDeviceClass.LOCK,
+                name=f"{vehicle.info.title} Charger",
+                device_class=BinarySensorDeviceClass.LOCK,
             ),
         )
         self._attr_unique_id = f"{vehicle.info.vin}_charger_locked"
@@ -122,3 +131,193 @@ class EnyaqBinarySensorChargerLocked(EnyaqBinarySensor):
             return "mdi:lock"
         else:
             return "mdi:lock-open"
+
+
+class EnyaqBinarySensorLocked(EnyaqBinarySensor):
+    def __init__(self, coordinator: DataUpdateCoordinator, vehicle: Vehicle) -> None:
+        super().__init__(
+            coordinator,
+            vehicle,
+            BinarySensorEntityDescription(
+                key="locked",
+                name=f"{vehicle.info.title} Locks",
+                device_class=BinarySensorDeviceClass.LOCK,
+            ),
+        )
+        self._attr_unique_id = f"{vehicle.info.vin}_locked"
+
+    @property
+    def is_on(self):
+        if not self.coordinator.data:
+            return None
+
+        self._update_device_from_coordinator()
+
+        return not self.vehicle.status.locked
+
+    @property
+    def icon(self):
+        if not self.coordinator.data:
+            return "mdi:lock"
+
+        self._update_device_from_coordinator()
+
+        if self.is_on:
+            return "mdi:lock-open"
+        else:
+            return "mdi:lock"
+
+
+class EnyaqBinarySensorDoorsLocked(EnyaqBinarySensor):
+    def __init__(self, coordinator: DataUpdateCoordinator, vehicle: Vehicle) -> None:
+        super().__init__(
+            coordinator,
+            vehicle,
+            BinarySensorEntityDescription(
+                key="doors_locked",
+                name=f"{vehicle.info.title} Doors Locks",
+                device_class=BinarySensorDeviceClass.LOCK,
+            ),
+        )
+        self._attr_unique_id = f"{vehicle.info.vin}_doors_locked"
+
+    @property
+    def is_on(self):
+        if not self.coordinator.data:
+            return None
+
+        self._update_device_from_coordinator()
+
+        return not self.vehicle.status.doors_locked
+
+    @property
+    def icon(self):
+        if not self.coordinator.data:
+            return "mdi:lock"
+
+        self._update_device_from_coordinator()
+
+        if self.is_on:
+            return "mdi:car-door-lock-open"
+        else:
+            return "mdi:car-door-lock"
+
+
+class EnyaqBinarySensorDoorsOpen(EnyaqBinarySensor):
+    def __init__(self, coordinator: DataUpdateCoordinator, vehicle: Vehicle) -> None:
+        super().__init__(
+            coordinator,
+            vehicle,
+            BinarySensorEntityDescription(
+                key="doors_open",
+                name=f"{vehicle.info.title} Doors",
+                device_class=BinarySensorDeviceClass.DOOR,
+                icon="mdi:car-door",
+            ),
+        )
+        self._attr_unique_id = f"{vehicle.info.vin}_doors_open"
+
+    @property
+    def is_on(self):
+        if not self.coordinator.data:
+            return None
+
+        self._update_device_from_coordinator()
+
+        return self.vehicle.status.doors_open
+
+
+class EnyaqBinarySensorWindowsOpen(EnyaqBinarySensor):
+    def __init__(self, coordinator: DataUpdateCoordinator, vehicle: Vehicle) -> None:
+        super().__init__(
+            coordinator,
+            vehicle,
+            BinarySensorEntityDescription(
+                key="windows_open",
+                name=f"{vehicle.info.title} Windows",
+                device_class=BinarySensorDeviceClass.WINDOW,
+                icon="mdi:car-door",
+            ),
+        )
+        self._attr_unique_id = f"{vehicle.info.vin}_windows_open"
+
+    @property
+    def is_on(self):
+        if not self.coordinator.data:
+            return None
+
+        self._update_device_from_coordinator()
+
+        return self.vehicle.status.windows_open
+
+
+class EnyaqBinarySensorTrunkOpen(EnyaqBinarySensor):
+    def __init__(self, coordinator: DataUpdateCoordinator, vehicle: Vehicle) -> None:
+        super().__init__(
+            coordinator,
+            vehicle,
+            BinarySensorEntityDescription(
+                key="trunk_open",
+                name=f"{vehicle.info.title} Trunk",
+                device_class=BinarySensorDeviceClass.OPENING,
+                icon="mdi:car",
+            ),
+        )
+        self._attr_unique_id = f"{vehicle.info.vin}_trunk_open"
+
+    @property
+    def is_on(self):
+        if not self.coordinator.data:
+            return None
+
+        self._update_device_from_coordinator()
+
+        return self.vehicle.status.trunk_open
+
+
+class EnyaqBinarySensorBonnetOpen(EnyaqBinarySensor):
+    def __init__(self, coordinator: DataUpdateCoordinator, vehicle: Vehicle) -> None:
+        super().__init__(
+            coordinator,
+            vehicle,
+            BinarySensorEntityDescription(
+                key="bonnet_open",
+                name=f"{vehicle.info.title} Bonnet",
+                device_class=BinarySensorDeviceClass.OPENING,
+                icon="mdi:car",
+            ),
+        )
+        self._attr_unique_id = f"{vehicle.info.vin}_bonnet_open"
+
+    @property
+    def is_on(self):
+        if not self.coordinator.data:
+            return None
+
+        self._update_device_from_coordinator()
+
+        return self.vehicle.status.bonnet_open
+
+
+class EnyaqBinarySensorLightsOn(EnyaqBinarySensor):
+    def __init__(self, coordinator: DataUpdateCoordinator, vehicle: Vehicle) -> None:
+        super().__init__(
+            coordinator,
+            vehicle,
+            BinarySensorEntityDescription(
+                key="lights_on",
+                name=f"{vehicle.info.title} Lights",
+                device_class=BinarySensorDeviceClass.LIGHT,
+                icon="mdi:car-light-high",
+            ),
+        )
+        self._attr_unique_id = f"{vehicle.info.vin}_lights_on"
+
+    @property
+    def is_on(self):
+        if not self.coordinator.data:
+            return None
+
+        self._update_device_from_coordinator()
+
+        return self.vehicle.status.lights_on
