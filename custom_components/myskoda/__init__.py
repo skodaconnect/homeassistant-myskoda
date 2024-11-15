8392100f7ab599e3b3f47383036450d8122e902b
Nils Vogels <n.vogels@aves-it.nl>
2024-11-15 23:15:37 +0100

    Remove mqtt and session when unloading

--- custom_components/myskoda/__init__.py
+++ custom_components/myskoda/__init__.py
@@ -65,6 +65,13 @@ async def async_setup_entry(hass: HomeAssistant, config: ConfigEntry) -> bool:
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
+
+    coordinators = hass.data[DOMAIN][entry.entry_id].get(COORDINATORS)
+    if coordinators:
+        for coord in coordinators:
+            await coord.myskoda.disconnect()
+            await coord.myskoda.session.close()
+
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
