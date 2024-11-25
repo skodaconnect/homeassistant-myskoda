4ffd85e534851fe8573899dfe5e6b41fcf9b760c
prvakt <74157307+prvakt@users.noreply.github.com>
2024-11-25 21:37:09 +0100

    Add button to generate fixtures into HA log (#345)
    * Add button to generate fixtures into HA log
    
    * deactivated when pressed, notified when generated, readme updated
    
    ---------
    
    Co-authored-by: Tomas Prvak <tprvak@cpicardgroup.com>

--- custom_components/myskoda/button.py
+++ custom_components/myskoda/button.py
@@ -8,15 +8,21 @@ from homeassistant.components.button import (
     ButtonEntity,
     ButtonEntityDescription,
 )
+from homeassistant.components.persistent_notification import (
+    async_create as async_create_persistent_notification,
+)
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import EntityCategory
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import DiscoveryInfoType  # pyright: ignore [reportAttributeAccessIssue]
 from homeassistant.util import Throttle
 
+from myskoda.models.fixtures import Endpoint
 from myskoda.models.info import CapabilityId
 
 from .const import API_COOLDOWN_IN_SECONDS, CONF_READONLY, COORDINATORS, DOMAIN
+from .coordinator import MySkodaDataUpdateCoordinator
 from .entity import MySkodaEntity
 from .utils import add_supported_entities
 
@@ -31,7 +37,7 @@ async def async_setup_entry(
 ) -> None:
     """Set up the button platform."""
     add_supported_entities(
-        available_entities=[HonkFlash, Flash],
+        available_entities=[HonkFlash, Flash, GenerateFixtures],
         coordinators=hass.data[DOMAIN][config.entry_id][COORDINATORS],
         async_add_entities=async_add_entities,
     )
@@ -82,3 +88,55 @@ class Flash(MySkodaButton):
 
     def required_capabilities(self) -> list[CapabilityId]:
         return [CapabilityId.HONK_AND_FLASH]
+
+
+class GenerateFixtures(MySkodaButton):
+    """Generate Fixtures."""
+
+    entity_description = ButtonEntityDescription(
+        key="generate_fixtures",
+        translation_key="generate_fixtures",
+        device_class=ButtonDeviceClass.IDENTIFY,
+        entity_category=EntityCategory.DIAGNOSTIC,
+    )
+
+    def __init__(
+        self,
+        coordinator: MySkodaDataUpdateCoordinator,
+        vin: str,
+    ):
+        super().__init__(coordinator, vin)
+        self._is_enabled = True  # Track whether the button is enabled
+
+    @property
+    def available(self) -> bool:
+        """Return whether the button is available."""
+        return self._is_enabled
+
+    @Throttle(timedelta(seconds=API_COOLDOWN_IN_SECONDS))
+    async def async_press(self) -> None:
+        if not self._is_enabled:
+            return  # Ignore presses when disabled
+
+        # Disable the button
+        self._is_enabled = False
+        self.async_write_ha_state()
+
+        try:
+            vin: list[str] = [self.vehicle.info.vin]
+            description = f"Fixtures for {self.vehicle.info.specification.model} {self.vehicle.info.specification.trim_level} {self.vehicle.info.specification.model_year}"
+
+            result = await self.coordinator.myskoda.generate_get_fixture(
+                self.vehicle.info.specification.model, description, vin, Endpoint.ALL
+            )
+            _LOGGER.info(f"{description}: {result.to_json()}")
+
+            async_create_persistent_notification(
+                self.hass,
+                title="Fixtures Generated",
+                message=f"{description} has been successfully generated, check the log.",
+            )
+        finally:
+            # Re-enable the button
+            self._is_enabled = True
+            self.async_write_ha_state()
