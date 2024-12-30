ba7789df112af87f3d1b0f5f04e2436889363684
EnergyX <74157307+prvakt@users.noreply.github.com>
2024-12-30 18:20:53 +0100

    remove AuxiliaryHeating from partial update for CitigoE IV (#491)

--- custom_components/myskoda/coordinator.py
+++ custom_components/myskoda/coordinator.py
@@ -153,7 +153,6 @@ class MySkodaDataUpdateCoordinator(DataUpdateCoordinator[State]):
                         self.vin,
                         [
                             CapabilityId.AIR_CONDITIONING,
-                            CapabilityId.AUXILIARY_HEATING,
                             CapabilityId.CHARGING,
                             CapabilityId.PARKING_POSITION,
                             CapabilityId.STATE,
