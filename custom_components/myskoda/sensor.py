skodaconnect/homeassistant-myskoda

commit 39794fc087641586f06e00d6eda8b10dc1856c3f

feat: work around API sending incorrect mileage (#767)
Fixes #766 (maybe)

The Skoda API sometimes returns an incorrect (old) mileage. This
workaround inspects the last recorded value, if there is one, and uses
that if it happens to be larged than the value returned by the API.

custom_components/myskoda/sensor.py
@@ -534,11 +534,26 @@ class Mileage(MySkodaSensor):
 
     @property
     def native_value(self) -> int | None:  # noqa: D102
+        """Calculate the mileage_in_km.
+
+        The API sometimes erroneously returns an old, lower value. Mileage should never go down.
+        To work around this we inspect the last state, if there is one, and return that if it is
+        larger than the value from the API.
+        """
+        last_value = 0
+        last_state = self.hass.states.get(self.entity_id)
+        if last_state and last_state.state:
+            last_value = int(last_state.state)
+
+        mileage_in_km = None
         if maint_report := self.vehicle.maintenance.maintenance_report:
-            return maint_report.mileage_in_km
+            mileage_in_km = maint_report.mileage_in_km
         # If the maint report does not have mileage, use vehicle health as fallback
         elif health := self.vehicle.health:
-            return health.mileage_in_km
+            mileage_in_km = health.mileage_in_km
+
+        if mileage_in_km:
+            return max(mileage_in_km, last_value)
 
 
 class InspectionInterval(MySkodaSensor):

commit 4796f29c742c211b80543a3fe0d6cb6386853493

fix: prevent value/type error when setting last mileage value
Introduced in #767

custom_components/myskoda/sensor.py
@@ -540,7 +540,10 @@ class Mileage(MySkodaSensor):
         last_value = 0
         last_state = self.hass.states.get(self.entity_id)
         if last_state and last_state.state:
-            last_value = int(last_state.state)
+            try:
+                last_value = int(last_state.state)
+            except (ValueError, TypeError):
+                pass  # value may initially be 'unavailable' or 'None'
 
         mileage_in_km = None
         if maint_report := self.vehicle.maintenance.maintenance_report: