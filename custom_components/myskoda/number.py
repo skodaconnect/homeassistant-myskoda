3307ba7e9efdc94a35a21b8d44a9573081ebde4a
Dmitry Fursov <2463426+fursov@users.noreply.github.com>
2025-08-28 19:57:31 +0300

    Use is_supported instead of required_capabilities

--- custom_components/myskoda/number.py
+++ custom_components/myskoda/number.py
@@ -112,8 +112,14 @@ class ChargeLimit(MySkodaNumber):
         finally:
             self._enable_number()
 
-    def required_capabilities(self) -> list[CapabilityId]:
-        return [CapabilityId.CHARGING, CapabilityId.EXTENDED_CHARGING_SETTINGS]
+    def is_supported(self) -> bool:
+        charge_limit_supported = self.vehicle.has_capability(
+            CapabilityId.EXTENDED_CHARGING_SETTINGS
+        ) or (
+            self.vehicle.has_capability(CapabilityId.CHARGING)
+            and not self.vehicle.has_capability(CapabilityId.CHARGING_MQB)
+        )
+        return charge_limit_supported
 
 
 class AuxiliaryHeaterDuration(MySkodaNumber, RestoreEntity):
